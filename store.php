f3e7d715fd695171699ecaa92622cee4e47aaaa1
Srinivasan8863 <shreersv333@gmail.com>
2020-01-20 23:20:51 +0530

    the following files are the rest api files which facilitates CRUD features

---
+++ store.php
@@ -0,0 +1,30 @@
+<?php
+    require 'connect.php';
+    $postdata = file_get_contents("php://input");
+    if (isset($postdata)&& !empty($postdata)) {
+        # code...
+        $request = json_decode($postdata);
+        if (trim($request->data->model)===''||(int)$request->data->price<1) {
+            # code...
+            return http_response_code(400);
+        }
+        $model = mysqli_real_escape_string($con, trim($request->data->model));
+        $price = mysqli_real_escape_string($con, trim($request->data->price));
+        $sql = "INSERT INTO `cars`(`id`,`model`,`price`) VALUES (null,'{$model}','{$price}')";
+
+        if(mysqli_query($con,$sql))
+        {
+            http_response_code(201);
+            $car = [
+            'model' => $model,
+            'price' => $price,
+            'id'    => mysqli_insert_id($con)
+            ];
+            echo json_encode(['data'=>$car]);
+        }
+        else
+        {
+            http_response_code(422);
+        }
+            }
+?>
